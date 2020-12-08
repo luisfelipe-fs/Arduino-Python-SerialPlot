7cbb72a23e9c8f3947450e6fa7361f1e9db5e2dc
luisfelipe-fs <luisfelipe.fsk@outlook.com>
2020-12-08 18:48:03 -0300

    fix: codigo desnecessario

--- SerialWriteData/SerialWriteData.ino
+++ SerialWriteData/SerialWriteData.ino
@@ -1,6 +1,3 @@
-const int MAX_ITERATIONS = 5000; // 5 segundos / Delay(1) s = 5000; 
-int count = 0;
-
 void setup() {
   Serial.begin(9600); // Nota: Baud < Frequência de captura (1ms+FLOPS)
   pinMode(A0, INPUT); // = CONECTE A SAÍDA DO FILTRO EM A0
@@ -11,5 +8,4 @@ void loop() {
   int x = analogRead(A0);
   Serial.println(String(t) + " " + String(x));
   delay(1);
-  count += 1;
 }
