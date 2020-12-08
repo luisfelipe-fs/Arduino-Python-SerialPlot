4248265efd947ed47061ea91d9f7aa687139d0a4
luisfelipe-fs <luisfelipe.fsk@outlook.com>
2020-12-08 01:39:57 -0300

    feat: salvar dados no txt

--- SerialPlot.py
+++ SerialPlot.py
@@ -1,45 +1,66 @@
+from __future__ import print_function
+
 import serial
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from collections import deque
 import threading
 
-timeList = deque(maxlen=1000)
-tempList = deque(maxlen=1000)
+timeList = deque(maxlen=5000)
+audioList = deque(maxlen=5000)
+stopThread = False
 
-s = serial.Serial('/dev/ttyACM0') # COLOCA A PORTA DO TEU ARDUINO AQUI
-for i in range(20): s.readline()
+s = serial.Serial('/dev/ttyACM0') # PORTA DO ARDUINO
+
+# Removendo lixo
+for i in range(100): line = s.readline()
 
 def getSerialData ():
-    while True:
-        global s
+    global s, stopThread
+
+    count = 0
+    maxCount = 1500 # ~15 segundos
+    while not stopThread and count < maxCount:
         try:
             line = s.readline()
-        except:
-            return
+        except UnicodeDecodeError:
+            print("Encontrou algum lixo: ", line)
+            continue
+
         try:
-            readt, readA = [int(x) for x in str(line, 'utf-8')[:-2].split()]
-            timeList.append(readt)
-            audioList.append(readA/1023.*5)
-        except:
-            pass
+            readt, readA = [int(x) for x in line.decode('utf-8')[:-2].split()]
+        except ValueError:
+            print("Encontrou algum lixo: ", line)
+            continue
+        
+        timeList.append(readt/1000.)
+        audioList.append(readA/1023.*5)
+        count += 1
 
 thread = threading.Thread(target=getSerialData)
 thread.start()
 
 def onlinePlot (i, timeList, tempList):
     ax.clear()
-    ax.set_title("Amplitude Monitor")
-    ax.set_xlabel("Time (ms)")
+    ax.set_title("Monitor")
+    ax.set_xlabel("Tempo (s)")
     ax.set_ylabel("Amplitude (dB)")
     ax.plot(timeList, tempList)
 
-print('Conected to %s.' % s.name)
+print('Conectado a porta %s.\n\n' % s.name)
 
 fig = plt.figure()
 ax = plt.axes()
 
 ani = animation.FuncAnimation(fig, onlinePlot, fargs=(timeList, audioList), interval=1)
-plt.show()
-
+try:
+    plt.show()
+except AttributeError:
+    stopThread = True
+    print("Fechou o plot.")
+    print("Salvando em arquivo...")
+    with open("Dados.txt", "w") as f:
+        f.writelines(["%s %s\n" % (t, a) for t, a in zip(timeList, audioList)])
+    print("Feito.")
+        
 s.close()
